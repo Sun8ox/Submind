5ff2179c6217c660d78cf31042020236b7346b3a
Sun8ox <simon_kubik@icloud.com>
2025-06-05 14:08:29 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,16 @@
 import { NextResponse } from "next/server";
 
 
-export default async function middleware(request) {
+// Exists but doesnt do anything
+// I actually never managed to get it working :/
+export async function middleware(request) {
+    const token = request.cookies.get("Submind.AuthToken");
 
-
+    if (!token) return NextResponse.redirect(new URL("/login?redirect=" + request.pathname, request.url));;
 
     return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/creator/:path*", "/user/:path*", "/admin/:path*"],
+};
