cd35c8f10a715fd78c29ce6ecec6b8c7e555e6f5
Sun8ox <simon_kubik@icloud.com>
2025-06-05 09:16:18 +0200

    Fix for Cloudflare

---
+++ src/app/api/videos/remove/route.js
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server';
+import { deleteVideo } from '@/lib/videos/videos';
+import { getUserData } from '@/lib/auth/user';
+import { getVideoInfoById } from '@/lib/videos/db';
+import { validateId  } from '@/lib/validate';
+
+export async function GET(request) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const cookieStore = await request.cookies;
+
+        const videoId = searchParams.get('videoId');
+        
+        // Validate videoId
+        if (!videoId) return NextResponse.json({ success: false, message: "Video ID is required." }, { status: 400 });
+        const { success: isValidId, message: idMessage } = validateId(videoId);
+        if (!isValidId) return NextResponse.json({ success: false, message: idMessage }, { status: 400 });
+
+        // Get user data from the auth token
+        const token = cookieStore.get('Submind.AuthToken');
+        const { success: getUserDataSuccess, message: getUserDataMessage, user} = await getUserData(token);
+        if (getUserDataSuccess === false) return NextResponse.json({ success: false, message: getUserDataMessage }, { status: 401 });
+
+        // Get video info from the database
+        const videoInfo = await getVideoInfoById(videoId);
+        if (!videoInfo) return NextResponse.json({ success: false, message: "Video not found." }, { status: 404 });
+
+        // Check if user is authorized to delete the video
+        if (user.role != "Admin" && user.id != videoInfo.author) return NextResponse.json({ success: false, message: "You are not authorized to delete this video." }, { status: 403 });
+        
+        const { success, message } = await deleteVideo(videoId);
+        if (success === false) return NextResponse.json({ success: false, message }, { status: 500 });
+
+
+        // Return success response
+        return NextResponse.json({ success: true, message: "Video deleted successfully." }, { status: 200 });
+    } catch (error) {
+        console.error("Error in GET request:", error);
+        return NextResponse.json({ success: false, message: "Internal server error." }, { status: 500 });
+    }
+}
